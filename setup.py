64173bb722aff496274403d979cef1f1bef69006
cominixo01 <lucasdutraluza@gmail.com>
2018-09-15 12:52:56 -0300

    Update

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ setup(
     version='1.0.0',
     author='cominixo',
     author_email='lucasdutraluza@gmail.com',
-    long_description=long_description
+    long_description=long_description,
+    install_requires=[
+        'requests==2.19.1'
+    ],
     description='A Python wrapper for the CrackWatch API',
     packages=[
         'pycrackwatch',
