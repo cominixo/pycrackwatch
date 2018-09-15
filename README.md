adf83799ce73a4dd4eee1395b9edea9db6d9df1f
cominixo01 <lucasdutraluza@gmail.com>
2018-09-15 12:46:03 -0300

    Added README and requirements

---
+++ README.md
@@ -0,0 +1,20 @@
+# pycrackwatch
+
+A simple wrapper for the crackwatch API (https://crackwatch.com/api)
+
+
+## Installing
+
+To install pycrackwatch just run
+
+```
+pip install pycrackwatch
+```
+
+## Testing
+
+The tests are located inside the tests folder, to run them with pytest use
+
+```
+py.test tests
+```
--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from distutils.core import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='pycrackwatch',
-    version='0.0.1',
+    version='1.0.0',
+    author='cominixo',
+    author_email='lucasdutraluza@gmail.com',
+    long_description=long_description
     description='A Python wrapper for the CrackWatch API',
     packages=[
         'pycrackwatch',
